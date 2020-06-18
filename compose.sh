d9ce923723480d2bec1bea8740fde2d87e2f3058
Assis Ngolo <figassis@gmail.com>
2020-06-18 15:09:47 +0100

    prod deployment

--- compose.sh
+++ compose.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker-compose down  --remove-orphans; docker-compose build && docker-compose up -d
+option=""
+if [ "$1" = "prod" ]; then
+    fname="-f docker-compose.prod.yaml"
+fi
+docker-compose $fname down  --remove-orphans; docker-compose $fname build && docker-compose $fname up -d
