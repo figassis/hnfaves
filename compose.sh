f49436680ad51d5baf05bfda213f7226c8e07275
Assis Ngolo <figassis@gmail.com>
2020-06-18 20:12:55 +0100

    scale app instances

--- compose.sh
+++ compose.sh
@@ -4,4 +4,4 @@ if [ "$1" = "prod" ]; then
     fname="-f docker-compose.prod.yaml"
 fi
 
-docker-compose $fname down  --remove-orphans; docker-compose $fname build && docker-compose $fname up -d --scale hnfaves=4
+docker-compose $fname down  --remove-orphans; docker-compose $fname build && docker-compose $fname up -d --scale app=4
