79ae87d79e07ea4148f93b585e1fa9d1197c0c17
Assis Ngolo <figassis@gmail.com>
2020-06-18 15:18:09 +0100

    remove app port env requirement

--- compose.sh
+++ compose.sh
@@ -3,4 +3,6 @@ option=""
 if [ "$1" = "prod" ]; then
     fname="-f docker-compose.prod.yaml"
 fi
+
+touch deploy/acme.json
 docker-compose $fname down  --remove-orphans; docker-compose $fname build && docker-compose $fname up -d
